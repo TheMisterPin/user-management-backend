f77a875b7d5802e10bda180d501b7ef3698a9b89
TheMisterPin <137160089+TheMisterPin@users.noreply.github.com>
2024-06-14 15:47:18 +0200

    cors

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,18 +1,25 @@
 import express from 'express'
+import cors from 'cors'
 import authRoutes from './auth-routes'
 import userRoutes from './user-routes'
 import authMiddleware from '../middleware/auth-middleware'
 
-const cors = require('cors')
-
 const app = express()
 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
-app.use(cors())
 
 app.use('/auth', authRoutes)
 app.use('/users', [authMiddleware], userRoutes)
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
 export default app
